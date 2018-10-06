ae1f6428f43b11970675c82b6b8b6c0823036b95
Oasys Okubo <oasysokubo@gmail.com>
2018-10-06 12:09:50 -0700

    init decl

---
+++ Driver.java
@@ -0,0 +1,21 @@
+
+public class Driver {
+	
+	// 32 bytes of memory expressed in 8-bit strings
+	String[] Memory = { 
+			
+			"01101011", 
+			
+	};
+	
+	// String bits are parsed into substrings for the Opcodes and Arguments
+	String[] Opcode = new String[32];
+	String[] Argument = new String[32];
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
